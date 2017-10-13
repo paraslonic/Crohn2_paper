afa37c0fdf70b5e423b59f15d2403d0aaa168bdc
paraslonic <paraslonic@gmail.com>
2017-10-13 18:43:51 +0300

    add

--- recombination/look.r
+++ recombination/look.r
@@ -1,40 +1,40 @@
 library("reshape2")
 library("data.table")
+library("gplots")
 
 t = fread("recombinations_recent.txt",skip=1)
 strains = fread("strains", col.names = c("num","strain"))
+lineages = fread("lineages", col.names = c("id","lineage"))
 
-t. = subset(t, t$RecipientStrain == 2)
-plot(t.$End, t.$DonorLineage+1, type = "n", ylim=c(0,5))
-for(i in 1:nrow(t.)){
-  lines(c(t.[i,]$Start, t.[i,]$End), 
-        c(t.[i,]$DonorLineage,t.[i,]$DonorLineage), 
-        col = t.[i,]$DonorLineage, lwd = 4)
-  
-}
-
-t. = subset(t, t$RecipientStrain == 1)
-for(i in 1:nrow(t.)){
-  lines(c(t.[i,]$Start, t.[i,]$End), 
-        c(t.[i,]$DonorLineage+0.2,t.[i,]$DonorLineage+0.2), 
-        col = t.[i,]$DonorLineage, lwd = 4)
-}
-
-
-t.[, sum(End - Start), by=DonorLineage]
-
-## global
-t
 t[,length:=End-Start]
 t[,sum(length),by=.(StrainName,DonorLineage)]
-hist(t$`log(BF)`, xlim = c(0,500), breaks  =1000)
-t.ok = t[`log(BF)` > 200]
-t.ok[,sum(length),by=.(RecipientStrain,DonorLineage)]
-View(t)
-#
-plot(t$length, t$`log(BF)`, log="xy")
-rec = t[, sum(End - Start), by=.(RecipientStrain,DonorLineage)]
-recm = dcast(rec, RecipientStrain ~ DonorLineage)
-recm[is.na(recm)] = 0
-library("gplots")
-heatmap.2(as.matrix(recm[,-1]), scale = "row")
+rec = as.data.frame(dcast(t, RecipientStrain ~ DonorLineage, fun.aggregate = sum, value.var = "length"))
+rec = rec[,-1]
+
+colnames(rec) = lineages[as.integer(colnames(rec))]$lineage
+rownames(rec) = strains[as.integer(rownames(rec))]$strain
+heatmap.2(as.matrix(rec), trace="none", 
+          col = colorRampPalette(c("black","dark orange","orange","yellow","white")),
+          margins=c(14,14), cexCol = 1.2, cexRow=1, Colv=FALSE, Rowv=FALSE, dendrogram = "none")
+
+
+ani = read.delim("oani.csv")
+rownames(ani) = ani$name
+ani = ani[,-1]
+as.numeric(ani)
+ani[is.na(ani)] = 0
+ani = ani+t(ani)
+diag(ani) = 100
+ani = apply(ani, c(1,2),as.numeric)
+ani = 100 - ani
+hc = as.dendrogram(hclust(as.dist(ani)))
+
+clade_order <- order.dendrogram(hc)
+clade_name <- labels(hc)
+clade_position <- data.frame(clade_name,clade_order)
+clade_position <- clade_position[order(clade_position$clade_order),]
+new_order <- match(clade_position$clade_name,row.names(rec))
+rec = rec[new_order,]
+heatmap.2(as.matrix(rec), trace="none", 
+          col = colorRampPalette(c("black","dark orange","orange","yellow","white")),
+          margins=c(14,14), cexCol = 1.2, cexRow=1, Colv=FALSE, Rowv=hc, dendrogram = "row")
